c746f1a78d374fd8070e9746edc3f9fbb4a8d07e
Stan <stanfalkenrich@gmail.com>
2025-05-30 18:25:16 +0200

    add initial project

---
+++ code-with-quarkus/src/main/frontend/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const App = (): ReactElement => {
+    return (
+        <div>
+            <h1>Hello there</h1>
+        </div>
+    );
+}
+
+export default App;
